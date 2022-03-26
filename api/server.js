102f21f9faf5c193c58622d363a5b18c33df3677
Christine Mendoza <christine.mendoza@unc.edu>
2022-03-25 23:19:33 -0400

    add api connection to mongodb

--- api/server.js
+++ api/server.js
@@ -10,16 +10,16 @@ const app = express();
 
 require("dotenv").config({ path: "./config.env" });
 
-// TODO: clean up and finish/fix this file
+// TODO: refine
 
-// mongoose
-//     .connect('mongodb://127.0.0.1:27017/mydatabase')
-//     .then((x) => {
-//         console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
-//     })
-//     .catch((err) => {
-//         console.error('Error connecting to mongo', err.reason)
-//     })
+mongoose
+    .connect(process.env.ATLAS_URI)
+    .then((x) => {
+        console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
+    })
+    .catch((err) => {
+        console.error('Error connecting to mongo', err.reason)
+    })
 
 app.use(
   bodyParser.urlencoded({
@@ -36,16 +36,9 @@ app.use('/app/', issueRoutes)
 
 app.use(express.json());
 
-app.use(require("./routes/record"));
-// get driver connection
-const dbo = require("./db/conn");
+app.use(issueRoutes);
  
 app.listen(port, () => {
-  // perform a database connection when server starts
-  // dbo.connectToServer(function (err) {
-  //   if (err) console.error(err);
- 
-  // });
   console.log(`Server is running on port: ${port}`);
 });
 
@@ -56,14 +49,14 @@ app.use((req, res, next) => {
 
 // Base Route
 app.get('/', (req, res) => {
-  res.send('invaild endpoint')
+  res.send('invalid endpoint')
 })
 
-// app.get('*', (req, res) => {
-//   res.sendFile(
-//       path.join(__dirname, 'dist/polaris/index.html'),
-//   )
-// })
+app.get('*', (req, res) => {
+  res.sendFile(
+      path.join(__dirname, 'dist/polaris/index.html'),
+  )
+})
 
 // error handler
 app.use(function (err, req, res, next) {
