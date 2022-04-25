18bf25a278c75852a9096f319e9aea975a35bd50
Christine Mendoza <christine.mendoza@unc.edu>
2022-04-25 11:26:31 -0400

    add extra user fields to user service

--- client/src/app/service/user/user.ts
+++ client/src/app/service/user/user.ts
@@ -2,4 +2,7 @@ export class User {
     _id!: String;
     firstName!: String;
     lastName!: String;
+    favoriteLocations!: [String];
+    issueInteractions!: [String];
+    issuesCreated!:[String]
 }
