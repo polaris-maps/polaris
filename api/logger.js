818122060a0450f25502311c88520d36240f2879
Christine Mendoza <christine.mendoza@unc.edu>
2022-04-27 06:01:57 -0400

    send more res/req info to logs

--- api/logger.js
+++ api/logger.js
@@ -7,15 +7,18 @@ require('winston-mongodb');  // expose `winston.transports.MongoDB`
 
 logger = expressWinston.logger({
     transports: [
-        // File transport; uncomment to debug
-        new winston.transports.File({
-            filename: 'winstonLogs.log',
-            format: winston.format.combine(
-                winston.format.timestamp({ format: 'MMM-DD-YYYY HH:mm:ss' }),
-                winston.format.align(),
-                winston.format.printf(info => `${info.level}: ${[info.timestamp]}: ${info.message}`),
-            )
-        }),
+        // Uncomment Console/File transports for debug
+        // // Console transport
+        // new winston.transports.Console(),
+        // // File transport
+        // new winston.transports.File({
+        //     filename: 'winstonLogs.log',
+        //     format: winston.format.combine(
+        //         winston.format.timestamp({ format: 'MMM-DD-YYYY HH:mm:ss' }),
+        //         winston.format.align(),
+        //         winston.format.printf(info => `${info.level}: ${[info.timestamp]}: ${info.message}`),
+        //     )
+        // }),
         // MongoDB transport
         new winston.transports.MongoDB({
             level: 'info',
@@ -28,12 +31,13 @@ logger = expressWinston.logger({
                 winston.format.timestamp(),
                 //convert logs to a json format for mongodb
                 winston.format.json()
-            )
+            ),
+            metaKey:'meta'
         })
     ],
     meta: true, // optional: control whether you want to log the meta data about the request (default to true)
-    msg: "HTTP {{req.method}} {{req.url}}", // optional: customize the default logging message. E.g. "{{res.statusCode}} {{req.method}} {{res.responseTime}}ms {{req.url}}"
-    expressFormat: true, // Use the default Express/morgan request formatting. Enabling this will override any msg if true. Will only output colors with colorize set to true
+    // msg: "HTTP {{req.method}} {{req.url}}", // optional: customize the default logging message. E.g. "{{res.statusCode}} {{req.method}} {{res.responseTime}}ms {{req.url}}"
+    expressFormat: false, // Use the default Express/morgan request formatting. Enabling this will override any msg if true. Will only output colors with colorize set to true
     colorize: false, // Color the text and status code, using the Express/morgan color palette (text: gray, status: default green, 3XX cyan, 4XX yellow, 5XX red).
     ignoreRoute: function (req, res) { return false; } // optional: allows to skip some log messages based on request and/or response
 })
